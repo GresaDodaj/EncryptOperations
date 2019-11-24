c52d8047dec8b2577a1dcec25ee2c955a64dc6a8
Gresa Dodaj <gresad@stud.ntnu.no>
2019-11-24 12:33:34 +0100

    Added ReadFile class
    -implemented readFile method with 2 parameters (filepath and Context-the Activity)
    -AES encryption works with the file too

---
+++ app/src/main/java/com/trying/myapplication/ReadFile.java
@@ -0,0 +1,47 @@
+package com.trying.myapplication;
+import android.content.Context;
+import android.util.Log;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+public class ReadFile {
+
+    private static final String TAG = "ssasss";
+
+    public static String readFile(String filepath, Context context){
+        File file=new File(filepath);
+
+        try {
+            //AssetManager aManager = selectFileSize.this.getAssets();
+            InputStream inputStream = context.getAssets().open("_7KBfile.txt");
+            final BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            final StringBuilder stringBuilder = new StringBuilder();
+            boolean done = false;
+            while (!done) {
+                final String line = reader.readLine();
+                done = (line == null);
+
+                if (line != null) {
+                    stringBuilder.append(line);
+                }
+            }
+            reader.close();
+            inputStream.close();
+            return  stringBuilder.toString();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Log.e(TAG, "readFile():" + e.getMessage() );
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.e(TAG, "readFile(): " + e.getMessage() );
+        }
+        return null;
+    }
+
+
+}
--- app/src/main/java/com/trying/myapplication/selectFileSize.java
+++ app/src/main/java/com/trying/myapplication/selectFileSize.java
@@ -1,23 +1,65 @@
 package com.trying.myapplication;
 
+import androidx.annotation.RequiresPermission;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
+import android.content.res.AssetManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 public class selectFileSize extends AppCompatActivity {
 
+
+
+    public selectFileSize() throws Exception {
+    }
+    private File file;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_file_size);
+        Button btn1 = (Button) findViewById(R.id.btn1KB);
+        final TextView txt1 = (TextView) findViewById(R.id.txtView);
+        file = new File(Environment.getExternalStorageDirectory(), "_7KBfile.txt");
 
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String test1 = "GRESA";
+                String text2 = ReadFile.readFile("_7KBfile.txt", selectFileSize.this);
+                try {
+                    String text3 = AES.encrypt(text2);
+                    String text4 = AES.decrypt(text3);
+                    txt1.setText(text4);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+               // txt1.setText(ReadFile.readFile("_7KBfile.txt", selectFileSize.this));
+                String test123 = ReadFile.readFile("_7KBfile.txt",selectFileSize.this);
+                Log.d("CREATION", ReadFile.readFile("_7KBfile.txt",selectFileSize.this));
+            }
+        });
 
     }
 
-    public void encryptFile(View view)
-    {
 
-    }
+    public void encryptFile(View view) {
+
+            }
+
+
+
 }
