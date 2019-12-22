76c1212999075fa1563ec28ecd2e8cb2147126a5
Gresa Dodaj <gresad@stud.ntnu.no>
2019-12-22 15:14:24 +0100

    Added initial vector

--- app/src/main/java/com/trying/myapplication/DES.java
+++ app/src/main/java/com/trying/myapplication/DES.java
@@ -1,34 +1,48 @@
 package com.trying.myapplication;
+import java.security.SecureRandom;
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
 import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 class DES {
-
-    private static byte[] initKey() throws Exception {
+    static byte[] initKey() throws Exception {
         KeyGenerator keyGenerator = KeyGenerator.getInstance("DES");
-        keyGenerator.init(64);
+        keyGenerator.init(64);//should be 16 or 24 bytes / 112 or 168 bits
         SecretKey secretKey = keyGenerator.generateKey();
         return secretKey.getEncoded();
     }
 
-    static void encrypt(byte[] input) throws Exception {
-        byte [] key = initKey();
+    static byte[] encrypt(byte[] input, byte[] key) throws Exception {
+
+        SecureRandom secureRandom = new SecureRandom();
         SecretKey secretKey = new SecretKeySpec(key, "DES");
-        Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
-        cipher.doFinal(input);
+
+        Cipher cipher = Cipher.getInstance("DES/CBC/PKCS7Padding");
+
+        byte[] iv = new byte[cipher.getBlockSize()];
+        secureRandom.nextBytes(iv);
+
+        IvParameterSpec ivParameterSpec = new IvParameterSpec(iv);
+        cipher.init(Cipher.ENCRYPT_MODE, secretKey,ivParameterSpec);
+
+        return cipher.doFinal(input);
     }
 
-  /*  static byte[] decrypt(byte[] encryptedInput, byte[] key) throws Exception {
-        SecretKey secretKey = new SecretKeySpec(key, "DESede");
-        Cipher cipher = Cipher.getInstance("DESede/CBC/PKCS5Padding");  //modi cbc, padding:
-        cipher.init(Cipher.DECRYPT_MODE, secretKey);
+    static byte[] decrypt(byte[] encryptedInput, byte[] key, byte[] iv) throws Exception {
+
+
+
+        SecretKey secretKey = new SecretKeySpec(key, "DES");
+        Cipher cipher = Cipher.getInstance("DES/CBC/PKCS5Padding");
+        cipher.init(Cipher.DECRYPT_MODE, secretKey, new IvParameterSpec(iv));
+
+
         return cipher.doFinal(encryptedInput);
-    }*/
-    
-    /*static String byte_to_string(byte[] result_in_bytes) {
+    }
+
+    static String byte_to_string(byte[] result_in_bytes) {
         StringBuilder builder = new StringBuilder();
         for (byte resultByte : result_in_bytes) {
             if (Integer.toHexString(0xFF & resultByte).length() == 1) {
@@ -38,6 +52,6 @@ class DES {
             }
         }
         return builder.toString();
-    }*/
+    }
 
 }
